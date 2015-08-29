163a9c681eecf330fb664f218c123952c3c2caf5
Filip <filip1323@gmail.com>
2015-08-29 17:28:53 +0200

    #12

---
+++ brain.js
@@ -0,0 +1,39 @@
+module.exports = function (Spawn) {
+
+  Spawn.prototype.unitCount = {
+    harvesters: 0,
+    builders: 0,
+    guards: 0
+  }
+  for(var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    var itsSpawn = creep.getSpawnBase();
+    if(creep.memory.role == 'harvester') {
+      itsSpawn.unitCount.harvesters++;
+      harvester(creep);
+    }
+
+    if(creep.memory.role == 'builder') {
+      itsSpawn.unitCount.builders++;
+      builder(creep);
+    }
+
+    if(creep.memory.role == 'guard') {
+      itsSpawn.unitCount.guards++;
+      builder(creep);
+    }
+  }
+  for(var i in Game.spawns) {
+    var spawn = Game.spawns[i];
+    if(spawn.unitCount.harvesters < 1){
+      spawn.createHarvesterCreep();
+    }else if(spawn.unitCount.guards < 1){
+      spawn.createGuardCreep();
+    }else if(spawn.unitCount.harvesters < 3){
+      spawn.createHarvesterCreep();
+    }else if(spawn.UnitCount.guards < 3){
+      spawn.createGuardCreep();
+    }
+  }
+
+}
--- main.js
+++ main.js
@@ -2,34 +2,8 @@ var harvester = require('harvester');
 var guard = require('guard');
 var builder = require('builder');
 var initBaseCommands = require('baseCommands');
+var brain = require('brain');
 
 var mainSpawnPoint = Game.spawns.Base;
 initBaseCommands(Spawn);
-
-var UnitCount = {
-  harvesters: 0,
-  builders: 0,
-  guards: 0
-}
-for(var name in Game.creeps) {
-  var creep = Game.creeps[name];
-
-  if(creep.memory.role == 'harvester') {
-    UnitCount.harvesters++;
-    harvester(creep);
-  }
-
-  if(creep.memory.role == 'builder') {
-    UnitCount.builders++;
-    builder(creep);
-  }
-
-  if(creep.memory.role == 'guard') {
-    UnitCount.guards++;
-    builder(creep);
-  }
-}
-
-if(UnitCount.harvesters < 3){
-  mainSpawnPoint.createHarvesterCreep();
-}
+brain(Spawn);
