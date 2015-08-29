d650491937797031808e2051a388e58a62226c65
Filip <filip1323@gmail.com>
2015-08-29 16:56:57 +0200

    #7

--- baseCommands.js
+++ baseCommands.js
@@ -4,21 +4,24 @@ module.exports = function (Spawn) {
     return this.memory.index++;
   };
   Spawn.prototype.createHarvesterCreep = function() {
+    var thisSpawn = this;
     var i = this.getNextIndex();
     var name = "harvester";
-    return this.createCreep([MOVE, CARRY, WORK], name+i, {role: "harvester", "index": i, baseSpawn : this});
+    return this.createCreep([MOVE, CARRY, WORK], name+i, {role: "harvester", index: i, baseSpawn : thisSpawn});
     console.log("Harvester created");
   };
   Spawn.prototype.createBuilderCreep = function() {
+    var thisSpawn = this;
     var i = this.getNextIndex();
     var name = "builder";
-    return this.createCreep([MOVE, CARRY, WORK], name+i, {role: "builder", "index": i, baseSpawn : this});
+    return this.createCreep([MOVE, CARRY, WORK], name+i, {role: "builder", index: i, baseSpawn : thisSpawn});
     console.log("Builder created");
   };
   Spawn.prototype.createGuardCreep = function() {
+    var thisSpawn = this;
     var i = this.getNextIndex();
     var name = "guard";
-    return this.createCreep([ATTACK, CARRY, MOVE], name+i, {role: "guard", "index": i, baseSpawn : this});
+    return this.createCreep([ATTACK, CARRY, MOVE], name+i, {role: "guard", index: i, baseSpawn : thisSpawn});
     console.log("Guard created");
   };
 }
